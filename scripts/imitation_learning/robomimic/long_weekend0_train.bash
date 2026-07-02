49eb7397173afffed402bef0e4d7ea394f6128c3
Ansh Gandhi <anshg@nvidia.com>
2026-07-02 16:19:52 -0700

    long weekend training configs

---
+++ scripts/imitation_learning/robomimic/long_weekend0_train.bash
@@ -0,0 +1,19 @@
+conda activate isaaclab_robomimic
+cd ~/Documents/Isaac-AutoData/submodules/IsaacLab-Arena/submodules/IsaacLab
+mkdir -p logs/long_weekend
+
+for i in {0..5}; do
+    ./isaaclab.sh -p scripts/imitation_learning/robomimic/train.py \
+        --config ./scripts/imitation_learning/robomimic/long_weekend/dp_0${i}.json \
+        --log_dir long_weekend \
+        --task long_weekend_0${i} \
+        --algo diffusion_policy \
+        --dataset ../../datasets/teleop/hubble_generated_new_preproc.hdf5
+done
+
+# 00: control
+# 01: diffusion steps = 5
+# 02: diffusion steps = 3
+# 03: action horizon = 4
+# 04: prediction horizon = 24
+# 05: prediction horizon = 32
---
+++ scripts/imitation_learning/robomimic/long_weekend1_train.bash
@@ -0,0 +1,26 @@
+conda activate isaaclab_robomimic
+cd ~/Documents/Isaac-AutoData/submodules/IsaacLab-Arena/submodules/IsaacLab
+mkdir -p logs/long_weekend
+
+for i in {0..5}; do
+    case $i in
+        3) dataset=../../datasets/teleop/hubble_generated_1000_preproc.hdf5 ;;
+        4) dataset=../../datasets/teleop/hubble_horizontal_grasp_generated_preproc.hdf5 ;;
+        5) dataset=../../datasets/teleop/hubble_extra_cam_generated_preproc.hdf5 ;;
+        *) dataset=../../datasets/teleop/hubble_generated_new_preproc.hdf5 ;;
+    esac
+
+    ./isaaclab.sh -p scripts/imitation_learning/robomimic/train.py \
+        --config ./scripts/imitation_learning/robomimic/long_weekend/dp_1${i}.json \
+        --log_dir long_weekend \
+        --task long_weekend_1${i} \
+        --algo diffusion_policy \
+        --dataset ${dataset}
+done
+
+# 10: add joint pos to obs
+# 11: add actions to obs
+# 12: add both joint pos and actions to obs
+# 13: train from 1000 demos
+# 14: train from new dataset
+# 15: use extra camera
