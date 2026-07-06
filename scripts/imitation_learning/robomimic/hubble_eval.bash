fcea4b21fd76aca013035bba3eeaebc868fef152
Ansh Gandhi <anshg@nvidia.com>
2026-07-06 16:38:47 -0700

    eval script for hubble runs

--- scripts/imitation_learning/robomimic/hubble_eval.bash
+++ scripts/imitation_learning/robomimic/hubble_eval.bash
@@ -3,11 +3,24 @@ cd ~/Documents/Isaac-AutoData/submodules/IsaacLab-Arena
 shopt -s globstar
 
 for i in {0..39}; do
+    case "$i" in
+        0|1|2|4|8|10|12|28|29|30)
+            continue
+            ;;
+    esac
+
     python isaaclab_arena/evaluation/policy_runner.py \
         --viz kit \
         --policy_type robomimic_stand \
         --robomimic_checkpoint submodules/IsaacLab/logs/hubble/hubble_${i}/diffusion_policy_hubble_pick_and_place/**/models/model_epoch_600.pth \
-        --num_episodes 10 \
+        --num_episodes 50 \
+        --enable_cameras \
+        hubble_g1_static_pick_and_place
+    python isaaclab_arena/evaluation/policy_runner.py \
+        --viz kit \
+        --policy_type robomimic_stand \
+        --robomimic_checkpoint submodules/IsaacLab/logs/hubble/hubble_${i}/diffusion_policy_hubble_pick_and_place/**/last.pth \
+        --num_episodes 50 \
         --enable_cameras \
         hubble_g1_static_pick_and_place
     echo "--------------------------------"
